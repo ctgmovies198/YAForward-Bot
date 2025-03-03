ba4c31eaf4d16b8780ec4d98554f920e82b948bd
ctgmovies198 <ctgmovies198@gmail.com>
2025-03-03 16:36:48 +0600

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://besuduso:0B4FAHGCcjxkj54m@cluster0.qhjtc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002381954054'))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "-1002393900827") # FORCE SUB channel link 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")  # FORCE SUB ON - OFF
 
